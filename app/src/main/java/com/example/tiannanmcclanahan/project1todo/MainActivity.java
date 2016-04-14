cecc0f15ced94d15312881372c38dd1291808a3b
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-04-14 10:14:51 -0700

    Adds LinkedList, Long Click for Delete

--- app/src/main/java/com/example/tiannanmcclanahan/project1todo/MainActivity.java
+++ app/src/main/java/com/example/tiannanmcclanahan/project1todo/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.tiannanmcclanahan.project1todo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    LinkedList <String> toDoList;
+    ArrayAdapter<String> lAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toDoList = new LinkedList<>();
+        toDoList.add("Grocery List");
+        toDoList.add("To-Do List");
+        lAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,toDoList);
+
+        ListView listView = (ListView)findViewById(R.id.existinglist);
+        listView.setAdapter(lAdapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                toDoList.remove(position);
+                lAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
 }
