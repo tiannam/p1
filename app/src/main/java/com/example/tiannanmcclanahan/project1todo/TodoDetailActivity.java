466d64e71523e45ddea01a3525224fd1959fc0ab
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-04-15 10:10:38 -0700

    Fixed Intent in second activity, set OnClickListener, added + button, and Toast to second activity

--- app/src/main/java/com/example/tiannanmcclanahan/project1todo/TodoDetailActivity.java
+++ app/src/main/java/com/example/tiannanmcclanahan/project1todo/TodoDetailActivity.java
@@ -1,35 +1,53 @@
 package com.example.tiannanmcclanahan.project1todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
 public class TodoDetailActivity extends AppCompatActivity {
 
-    LinkedList<String> itemList;
+    LinkedList<String> itemList = new LinkedList<>();
 
     ArrayAdapter<String> iAdapter;
     EditText itemText;
     TextView textView;
+    ImageButton iAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_detail);
-        String eggs = getIntent().getStringExtra("eggs");
-        textView.setText(String.valueOf(eggs));
+//        String eggs = getIntent().getStringExtra("eggs");
+//        textView.setText(String.valueOf(eggs));
         iAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,itemList);
+
+        iAddButton = (ImageButton)findViewById(R.id.additembutton);
+        itemText = (EditText)findViewById(R.id.detaillist);
+        itemText.setText("");
 
         ListView itemView = (ListView) findViewById(R.id.tododetail);
         textView = (TextView)findViewById(R.id.oldlist);
         itemView.setAdapter(iAdapter);
+
+        itemView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intentI = new Intent(getApplicationContext(), TodoDetailActivity.class);
+                iAdapter.notifyDataSetChanged();
+                startActivity(intentI);
+                return;
+            }
+        });
 
         itemView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -39,5 +57,19 @@ public class TodoDetailActivity extends AppCompatActivity {
                 return false;
             }
         });
+        iAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = itemText.getText().toString();
+                if (input.length() > 0) {
+                    itemList.add(input);
+                    iAdapter.notifyDataSetChanged();
+                    itemText.setText("");
+                }else {
+                    Toast.makeText(getApplicationContext(), "Enter an item.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
